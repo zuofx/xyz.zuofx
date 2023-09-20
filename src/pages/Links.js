9c8655ebe327b4d6d4c6dcd0bdc28219d5ff0d75
Huy T <huytruong.eq@gmail.com>
2023-09-20 01:28:07 -0400

    Links Page and Assets

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,18 +1,64 @@
 import '../css/Links.css';
+import React, { useState, useEffect } from 'react';
 
 const Links = () => {
-    return (
-        <div className="Links">
-            <h1 className="WIP">
-                WIP
-                <br/>
-                
-            </h1>
-            <h2 className="WIP2">
-                Come back later!
-            </h2>
-        </div>
-    );
+    const [links, setLinks] = useState([
+
+    ])
+
+    const getData=()=>{
+        fetch('../data/links.json'
+        ,{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+          .then(function(response){
+            console.log(response)
+            return response.json();
+          })
+    
+          .then(function(myJson) {
+            console.log(myJson);
+            setLinks(myJson);
+          });
+      }
+  
+    useEffect(() => {
+        getData();
+    }, [])
+
+    if (links != null && links.length > 0) {
+        return (
+            <div className="Links">
+                <div className="linkButtons">
+                    {links.map((props) => {
+                        return(
+                            <a href={props.link} target='_blank' rel="noreferrer">
+                                <div className="linkCard">
+                                    <div className="linkCardElements">
+                                        <img src={require(`../assets/link-cards/Logos/${props.img}`)} alt="logo"></img>
+                                        <h1>
+                                            {props.name}
+                                        </h1>
+                                    </div>
+                                </div>
+                            </a>
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }else {
+        return(
+            <div className="Links">
+                Nothing to display here.
+            </div>
+        )
+    }
+    
 };
 
 export default Links;
